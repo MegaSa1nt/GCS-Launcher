5897df5bd4204b1fb351a70d0033efa6b4d60d95
i10lusha <147263186+Unitoshka@users.noreply.github.com>
2024-05-11 00:37:09 +0500

    основной блок для запуска игры

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { Home, Upload } from 'lucide-svelte';
+	import { Settings } from 'lucide-svelte';
 	import style from './style.module.scss';
+	import game from '$lib/images/game.png';
 </script>
 
 <svelte:head>
@@ -8,9 +9,33 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class={style.title}>
-	EronDonDonServer
+<div class={style.contentBlock}>
+	<div class={style.head}>
+		<div class={style.title}>
+			EronDonDonServer
+		</div>
+		<div class={style.description}>
+			Привет, сосунок!
+		</div>
+	</div>
+
+	<div class={style.launchBlock}>
+		<div class={style.launchContent}>
+			<img src={game} alt="ИГРУЛЕЧКА" class={style.gamePicture}>
+			<div class={style.loadGame}>
+				<div class={style.loadButton}>
+					Играть
+				</div>
+				<div class={style.settingsButton}>
+					<Settings color='#FFFFFF' size={35} />
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
+
+
+
 
 <style>
 
