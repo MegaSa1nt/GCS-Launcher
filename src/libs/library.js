994713b96ce971c1a25bb3a0522b1d0ccf68d78b
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-17 03:41:33 +0300

    Теперь лаунчер умеет сохранять последнее обновление и скачивать патчи. Не патчить))

--- src/libs/library.js
+++ src/libs/library.js
@@ -13,15 +13,26 @@ window.playButtonState = 0;
 
 window.updatePlayButtonState = () => {
 	switch(true) {
-		case isCheckingUpdate:
-			window.gameUpdatingAnimation = '';
-			window.playButtonIsAvailable = style.isAvailable;
-			window.playButtonState = 1;
-			break;
 		case isUpdatingGame:
 			window.gameUpdatingAnimation = style.show;
 			window.playButtonIsAvailable = '';
 			window.playButtonState = 1;
+			break;
+		case isCheckingUpdate:
+		case isGameStarting:
+			window.gameUpdatingAnimation = '';
+			window.playButtonIsAvailable = style.isAvailable;
+			window.playButtonState = 1;
+			break;
+		case isPendingUpdate:
+			window.gameUpdatingAnimation = '';
+			window.playButtonIsAvailable = style.isAvailable;
+			window.playButtonState = 2;
+			break;
+		case isGameRunning:
+			window.gameUpdatingAnimation = style.show;
+			window.playButtonIsAvailable = '';
+			window.playButtonState = 0;
 			break;
 		default:
 			window.gameUpdatingAnimation = '';
@@ -39,6 +50,10 @@ library.initializeEvents = function() {
 	if(typeof window.isUpdatingGame == 'undefined') window.isUpdatingGame = false;
 	if(typeof window.isGameStarting == 'undefined') window.isGameStarting = false;
 	if(typeof window.isGameRunning == 'undefined') window.isGameRunning = false;
+	if(typeof window.isPendingUpdate == 'undefined') window.isPendingUpdate = false;
+	
+	// Not really events
+	if(typeof window.new_updates == 'undefined') window.new_updates = [];
 }
 
 library.initializeVariables = function() {
@@ -62,37 +77,48 @@ library.checkUpdates = async function() {
 	if(settings.update_time == 0) {
 		console.log('You should install game ;)');
 		await library.changeIsCheckingUpdateState(false);
+		await library.changePendingUpdateState(true);
 	} else {
 		fetch(settings.updates_api_url + "updates/" + settings.update_time).then(res => res.json()).then(response => {
 			if(response.length == 0) {
 				console.log("No updates available. Latest version!");
 				library.changeIsCheckingUpdateState(false);
+				library.changePendingUpdateState(false);
 				return true;
+			} else {
+				console.log("Updates were found!");
+				window.new_updates = response;
+				library.changeIsCheckingUpdateState(false);
+				library.changePendingUpdateState(true);
 			}
 		}).catch(err => {
 			console.error('Failed checking updates:', err);
 			library.changeIsCheckingUpdateState(false);
+			library.changePendingUpdateState(false);
 		});
 	}
 }
 
 library.installGame = async function() {
 	if(window.isUpdatingGame) return;
+	//return console.log('Tried to install game.');
 	library.changeUpdatingGameState(true);
+	library.changePendingUpdateState(false);
 	const settings = library.getSettings();
+	const lastUpdateTimestamp = await library.getLatestUpdateTimestamp();
 	const configPath = await resolve(await appCacheDir() + "/temp.7z");
 	const resourcePath = "C:\\Users\\megas\\OneDrive\\Рабочий стол\\UNPACKING TEST";
 	//const resourcePath = await resourceDir();
 	console.log('Starting downloading game...');
-	return;
 	invoke('download_archive', { url: settings.updates_api_url + "download/0", tempPath: configPath}).then(stdout => {
 		if(stdout === null) {
 			console.log('Unpacking game...');
 			invoke("unpack_archive", { archivePath: configPath, extractPath: resourcePath}).then(stdout => {
 				if(stdout === null) {
-					console.log('Game successfully updated!');
+					console.log('Game successfully downloaded!');
 					library.changeUpdatingGameState(false);
 					library.cleanTemporaryFiles();
+					localStorage.update_time = lastUpdateTimestamp;
 				} else {
 					console.error('Failed extracting archive:', err);
 					library.changeUpdatingGameState(false);
@@ -146,13 +172,96 @@ library.changeIsGameRunningState = async function(state) {
 	document.dispatchEvent(playButtonStateChangeEvent);
 }
 
+library.changePendingUpdateState = async function(state) {
+	window.isPendingUpdate = state;
+	await window.updatePlayButtonState();
+	document.dispatchEvent(playButtonStateChangeEvent);
+}
+
 library.openOrInstallGame = async function() {
+	if(isGameRunning) return;
+	if(isPendingUpdate) return library.updateGame();
+	await library.changeIsGameStartingState(true);
 	await open("GreenCatsServer.exe").then(res => {
+		library.changeIsGameStartingState(false);
+		library.changeIsGameRunningState(true);
 		console.log(res);
 	}).catch(err => {
-		console.log("No game LOL:", err);
-		library.installGame();
+		library.changeIsGameStartingState(false);
+		library.changeIsGameRunningState(false);
+		console.log("Failed to run game:", err);
+		library.updateGame();
 	})
+}
+
+library.updateGame = async function() {
+	if(window.isUpdatingGame) return;
+	const settings = library.getSettings();
+	console.log(new_updates);
+	if(settings.update_time == 0) return library.installGame();
+	library.changePendingUpdateState(false);
+	library.changeUpdatingGameState(true);
+	var i = 0;
+	for(i = 0; i < new_updates.length; i++) {
+		await library.patchGame(new_updates[i]);
+	}
+	const lastUpdateTimestamp = new_updates[new_updates.length - 1];
+	console.log('Game successfully updated!');
+	library.changeUpdatingGameState(false);
+	library.cleanTemporaryFiles();
+	localStorage.update_time = lastUpdateTimestamp;
+}
+
+library.getLatestUpdateTimestamp = async function() {
+	const settings = library.getSettings();
+	return new Promise(r => {
+		fetch(settings.updates_api_url + "lastUpdate").then(res => res.json()).then(response => {
+			r(response.timestamp);
+		}).catch(err => {
+			console.error('Failed getting update time:', err);
+			r(0);
+		});
+	});
+}
+
+library.patchGame = async function(patchTimestamp) {
+	const settings = library.getSettings();
+	const patchArchivePath = await resolve(await appCacheDir() + "/patch_" + patchTimestamp + ".7z");
+	const patchFolderPath = await resolve(await appCacheDir() + "/patch_" + patchTimestamp);
+	console.log('Downloading patch ' + patchTimestamp + '...');
+	return new Promise(r => {
+		invoke('download_archive', { url: settings.updates_api_url + "download/" + patchTimestamp, tempPath: patchArchivePath}).then(stdout => {
+			if(stdout === null) {
+				console.log('Unpacking patch ' + patchTimestamp + '...');
+				invoke("unpack_archive", { archivePath: patchArchivePath, extractPath: patchFolderPath}).then(stdout => {
+					if(stdout === null) {
+						console.log('Patching ' + patchTimestamp + '...');
+						r(true);
+					} else {
+						console.error('Failed extracting archive:', err);
+						library.changeUpdatingGameState(false);
+						library.cleanTemporaryFiles();
+						r(false);
+					}
+				}).catch(err => {				
+					console.error('Failed extracting archive:', err);
+					library.changeUpdatingGameState(false);
+					library.cleanTemporaryFiles();
+					r(false);
+				});
+			} else {
+				console.error('Failed downloading archive:', stdout);
+				library.changeUpdatingGameState(false);
+				library.cleanTemporaryFiles();
+				r(false);
+			}
+		}).catch(err => {
+			console.error('Failed downloading archive:', err);
+			library.changeUpdatingGameState(false);
+			library.cleanTemporaryFiles();
+			r(false);
+		});
+	});
 }
 
 library.styles = style;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,13 @@
 	import PlayButtonIcon from '../components/PlayButtonIcon/playButtonIcon.svelte';
 	
 	export let buttonState = window.playButtonState;
+	export let buttonIsAvailable = window.playButtonIsAvailable;
+	export let updatingAnimation = window.gameUpdatingAnimation;
 	
 	document.addEventListener("playButtonStateChange", function(event) {
-		buttonState = updatePlayButtonState();
+		buttonState = window.playButtonState;
+		buttonIsAvailable = window.playButtonIsAvailable;
+		updatingAnimation = window.gameUpdatingAnimation;
 	});
 	updatePlayButtonState();
 </script>
@@ -30,8 +34,8 @@
 	<div class={style.launchBlock}>
 		<div class={style.launchContent}>
 			<div class={style.loadGame}>
-				<div on:click={() => library.openOrInstallGame()} class={[style.loadButton, playButtonIsAvailable].join(' ')}>
-					<span id="play-button-animation" class={[style.loadAnimation, gameUpdatingAnimation].join(' ')}></span>
+				<div on:click={() => library.openOrInstallGame()} class={[style.loadButton, buttonIsAvailable].join(' ')}>
+					<span id="play-button-animation" class={[style.loadAnimation, updatingAnimation].join(' ')}></span>
 					<PlayButtonIcon state={buttonState} />
 				</div>
 				<div class={style.settingsButton}>
