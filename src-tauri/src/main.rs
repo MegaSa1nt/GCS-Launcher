88278647f886c3741f24c271df5d11412db0eace
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-17 09:22:43 +0300

    Патчи работают. Система обновлений полностью готова, WWW

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,9 @@ use std::fs::File;
 use std::io::Write;
 
 #[tauri::command]
-async fn download_archive(url: String, temp_path: String) -> Result<(), String> {
-    let response = reqwest::get(&url).await.expect("Failed downloading archive");
-	let body = response.bytes().await.expect("Failed downloading archive");
+async fn download_file(url: String, temp_path: String) -> Result<(), String> {
+    let response = reqwest::get(&url).await.expect("Failed downloading file");
+	let body = response.bytes().await.expect("Failed downloading file");
     let path = Path::new(&temp_path);
 
     let file = match File::create(&path) {
@@ -28,7 +28,7 @@ fn main() {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_shell::init())
 		.plugin(tauri_plugin_fs::init())
-		.invoke_handler(tauri::generate_handler![download_archive, unpack_archive])
+		.invoke_handler(tauri::generate_handler![download_file, unpack_archive])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
--- src/libs/library.js
+++ src/libs/library.js
@@ -1,11 +1,10 @@
 import { invoke } from '@tauri-apps/api/core';
-import { appCacheDir, resolve, resourceDir } from '@tauri-apps/api/path';
-import { remove } from '@tauri-apps/plugin-fs';
-import { open } from '@tauri-apps/plugin-shell';
+import { appCacheDir, resolve, resourceDir, join } from '@tauri-apps/api/path';
+import { remove, readDir, BaseDirectory, rename } from '@tauri-apps/plugin-fs';
+import { open, Command } from '@tauri-apps/plugin-shell';
 import style from './style.module.scss';
 const library = [];
 let playButtonStateChangeEvent = new Event("playButtonStateChange", {bubbles: true});
-
 
 window.gameUpdatingAnimation = '';
 window.playButtonIsAvailable = style.isAvailable;
@@ -62,11 +61,14 @@ library.initializeVariables = function() {
 
 library.getSettings = function() {
 	library.initializeVariables();
+	const resourcePath = "C:\\Users\\megas\\OneDrive\\Рабочий стол\\UNPACKING TEST";
+	//const resourcePath = await resourceDir();
 	return {
 		updates_api_url: "https://updates.gcs.icu/",
 		gdps_name: "GreenCatsServer",
 		
-		update_time: localStorage.update_time
+		update_time: localStorage.update_time,
+		resource_path: resourcePath
 	}
 }
 
@@ -101,19 +103,16 @@ library.checkUpdates = async function() {
 
 library.installGame = async function() {
 	if(window.isUpdatingGame) return;
-	//return console.log('Tried to install game.');
 	library.changeUpdatingGameState(true);
 	library.changePendingUpdateState(false);
 	const settings = library.getSettings();
 	const lastUpdateTimestamp = await library.getLatestUpdateTimestamp();
 	const configPath = await resolve(await appCacheDir() + "/temp.7z");
-	const resourcePath = "C:\\Users\\megas\\OneDrive\\Рабочий стол\\UNPACKING TEST";
-	//const resourcePath = await resourceDir();
 	console.log('Starting downloading game...');
-	invoke('download_archive', { url: settings.updates_api_url + "download/0", tempPath: configPath}).then(stdout => {
+	invoke('download_file', { url: settings.updates_api_url + "download/0", tempPath: configPath}).then(stdout => {
 		if(stdout === null) {
 			console.log('Unpacking game...');
-			invoke("unpack_archive", { archivePath: configPath, extractPath: resourcePath}).then(stdout => {
+			invoke("unpack_archive", { archivePath: configPath, extractPath: settings.resource_path}).then(stdout => {
 				if(stdout === null) {
 					console.log('Game successfully downloaded!');
 					library.changeUpdatingGameState(false);
@@ -185,7 +184,6 @@ library.openOrInstallGame = async function() {
 	await open("GreenCatsServer.exe").then(res => {
 		library.changeIsGameStartingState(false);
 		library.changeIsGameRunningState(true);
-		console.log(res);
 	}).catch(err => {
 		library.changeIsGameStartingState(false);
 		library.changeIsGameRunningState(false);
@@ -197,7 +195,6 @@ library.openOrInstallGame = async function() {
 library.updateGame = async function() {
 	if(window.isUpdatingGame) return;
 	const settings = library.getSettings();
-	console.log(new_updates);
 	if(settings.update_time == 0) return library.installGame();
 	library.changePendingUpdateState(false);
 	library.changeUpdatingGameState(true);
@@ -230,12 +227,36 @@ library.patchGame = async function(patchTimestamp) {
 	const patchFolderPath = await resolve(await appCacheDir() + "/patch_" + patchTimestamp);
 	console.log('Downloading patch ' + patchTimestamp + '...');
 	return new Promise(r => {
-		invoke('download_archive', { url: settings.updates_api_url + "download/" + patchTimestamp, tempPath: patchArchivePath}).then(stdout => {
+		invoke('download_file', { url: settings.updates_api_url + "download/" + patchTimestamp, tempPath: patchArchivePath}).then(stdout => {
 			if(stdout === null) {
 				console.log('Unpacking patch ' + patchTimestamp + '...');
-				invoke("unpack_archive", { archivePath: patchArchivePath, extractPath: patchFolderPath}).then(stdout => {
+				invoke("unpack_archive", { archivePath: patchArchivePath, extractPath: patchFolderPath}).then(async function(stdout) {
 					if(stdout === null) {
 						console.log('Patching ' + patchTimestamp + '...');
+						const patchFiles = await library.recursiveReadDir(patchFolderPath, patchFolderPath);
+						var i = 0;
+						for(i = 0; i < patchFiles.length; i++) {
+							var patchFunction = patchFiles[i].slice(-2);
+							var patchFile = patchFiles[i].slice(0, patchFiles[i].length - 2);
+							const patchPath = await join(settings.resource_path, patchFile);
+							const fullPatchPath = await join(patchFolderPath, patchFile + ".p");
+							switch(patchFunction) {
+								case '.p':
+									const check = await Command.create("bin/hpatch.exe", [patchPath, fullPatchPath, patchPath + "_new"], { encoding: "utf-8" }).execute();
+									await remove(patchPath).catch(err => {console.error(err);});
+									await rename(patchPath + "_new", patchPath).catch(err => {console.error(err);});
+									console.log("Patched", patchFile);
+									break;
+								case '.m':
+									await invoke('download_file', { url: settings.updates_api_url + "download/" + patchFile, tempPath: patchPath});
+									console.log("Downloaded", patchFile);
+									break;
+								case '.d':
+									await remove(patchPath).catch(err => {console.error(err);});
+									console.log("Removed", patchFile);
+									break;
+							}
+						}
 						r(true);
 					} else {
 						console.error('Failed extracting archive:', err);
@@ -262,6 +283,20 @@ library.patchGame = async function(patchTimestamp) {
 			r(false);
 		});
 	});
+}
+
+library.recursiveReadDir = async function(parent, initialParent) {
+	const dirEntries = await readDir(parent, { baseDir: BaseDirectory.Cache });
+	const patchFiles = [];
+	for(const entry of dirEntries) {
+		const onlyFilePath = parent.substr(initialParent.length);
+		patchFiles.push(onlyFilePath + entry.name);
+		if(entry.isDirectory) {
+			const dir = await join(parent, entry.name);
+			library.recursiveReadDir(dir, initialParent);
+		}
+	}
+	return patchFiles;
 }
 
 library.styles = style;
