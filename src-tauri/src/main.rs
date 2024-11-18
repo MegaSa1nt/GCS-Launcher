69868edc8e5b070cfe86c82ff05b1c7669175b18
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-18 05:56:47 +0300

    Теперь лаунчер сохраняет контрольную сумму файлов
    Чтобы можно было проверить целостность файлов игры

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::path::Path;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+use sysinfo::System;
+use std::ffi::OsStr;
+
+use chksum_md5;
 
 #[tauri::command]
 async fn download_file(url: String, temp_path: String) -> Result<(), String> {
     let response = reqwest::get(&url).await.expect("Failed downloading file");
-	let body = response.bytes().await.expect("Failed downloading file");
+    let body = response.bytes().await.expect("Failed downloading file");
     let path = Path::new(&temp_path);
 
     let file = match File::create(&path) {
@@ -20,15 +25,33 @@ async fn download_file(url: String, temp_path: String) -> Result<(), String> {
 
 #[tauri::command]
 async fn unpack_archive(archive_path: String, extract_path: String) -> Result<(), String> {
-	sevenz_rust::decompress_file(&archive_path, &extract_path).expect("complete");
-	Ok(())
+    sevenz_rust::decompress_file(&archive_path, &extract_path).expect("complete");
+    Ok(())
+}
+
+#[tauri::command]
+async fn get_file_md5(file_path: String) -> Result<String, String> {
+	let path = PathBuf::from(file_path);
+	let digest = chksum_md5::chksum(path).expect("MD5 failed");
+	Ok(digest.to_hex_lowercase())
+}
+
+#[tauri::command]
+async fn check_processes(process: String) -> Result<(), String> {
+    let s = System::new_all();
+	let os_str = OsStr::new(&process);
+    for _process in s.processes_by_exact_name(&os_str) {
+        return Ok(());
+    }
+    return Err("No process with that name was found".to_string());
 }
 
 fn main() {
-	tauri::Builder::default()
-		.plugin(tauri_plugin_shell::init())
-		.plugin(tauri_plugin_fs::init())
-		.invoke_handler(tauri::generate_handler![download_file, unpack_archive])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .invoke_handler(tauri::generate_handler![download_file, unpack_archive, get_file_md5, check_processes])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/libs/library.js
+++ src/libs/library.js
@@ -1,7 +1,8 @@
 import { invoke } from '@tauri-apps/api/core';
-import { appCacheDir, resolve, resourceDir, join } from '@tauri-apps/api/path';
+import { appCacheDir, resolve, resourceDir, join, sep } from '@tauri-apps/api/path';
 import { remove, readDir, BaseDirectory, rename } from '@tauri-apps/plugin-fs';
 import { open, Command } from '@tauri-apps/plugin-shell';
+import Database from '@tauri-apps/plugin-sql';
 import style from './style.module.scss';
 const library = [];
 let playButtonStateChangeEvent = new Event("playButtonStateChange", {bubbles: true});
@@ -44,7 +45,7 @@ window.updatePlayButtonState = () => {
 
 library.sleep = ms => new Promise(r => setTimeout(r, ms));
 
-library.initializeEvents = function() {
+library.initializeEvents = async function() {
 	if(typeof window.isCheckingUpdate == 'undefined') window.isCheckingUpdate = false;
 	if(typeof window.isUpdatingGame == 'undefined') window.isUpdatingGame = false;
 	if(typeof window.isGameStarting == 'undefined') window.isGameStarting = false;
@@ -53,7 +54,25 @@ library.initializeEvents = function() {
 	
 	// Not really events
 	if(typeof window.new_updates == 'undefined') window.new_updates = [];
+	if(typeof window.recursive_check == 'undefined') window.recursive_check = [];
+	if(typeof window.game_folders == 'undefined') window.game_folders = [];
+	const dbPath = await window.__TAURI__.path.resolve(await window.__TAURI__.path.appCacheDir(), "files.db")
+	if(typeof window.db == 'undefined') window.db = await Database.load("sqlite:" + dbPath);
+	await db.execute(`CREATE TABLE IF NOT EXISTS 'files' (
+		'file' varchar(255) NOT NULL DEFAULT '',
+		'md5' varchar(255) NOT NULL DEFAULT '',
+		PRIMARY KEY ('file')
+	);
+	CREATE TABLE IF NOT EXISTS 'folders' (
+		'folder' varchar(255) NOT NULL DEFAULT '',
+		PRIMARY KEY ('folder')
+	);`);
 }
+
+const gameCheckInterval = setInterval(async function() {
+	const settings = library.getSettings();
+	library.checkProcess(settings.game_exe);
+},	500);
 
 library.initializeVariables = function() {
 	if(typeof localStorage.update_time == 'undefined') localStorage.update_time = 0;
@@ -66,6 +85,7 @@ library.getSettings = function() {
 	return {
 		updates_api_url: "https://updates.gcs.icu/",
 		gdps_name: "GreenCatsServer",
+		game_exe: "GeometryDash.exe",
 		
 		update_time: localStorage.update_time,
 		resource_path: resourcePath
@@ -112,8 +132,10 @@ library.installGame = async function() {
 	invoke('download_file', { url: settings.updates_api_url + "download/0", tempPath: configPath}).then(stdout => {
 		if(stdout === null) {
 			console.log('Unpacking game...');
-			invoke("unpack_archive", { archivePath: configPath, extractPath: settings.resource_path}).then(stdout => {
+			invoke("unpack_archive", { archivePath: configPath, extractPath: settings.resource_path}).then(async function(stdout) {
 				if(stdout === null) {
+					console.log('Adding all files to SQL... (that means it also calculates MD5 checksum for all files)');
+					await library.addFolderToSQL(settings.resource_path);
 					console.log('Game successfully downloaded!');
 					library.changeUpdatingGameState(false);
 					library.cleanTemporaryFiles();
@@ -140,11 +162,14 @@ library.installGame = async function() {
 	});
 }
 
-library.cleanTemporaryFiles = async function() {
-	if(window.isUpdatingGame) return;
+library.cleanTemporaryFiles = async function(patchTimestamp = 0) {
 	const settings = library.getSettings();
-	const configPath = await resolve(await appCacheDir() + "/temp.7z");
-	await remove(configPath).catch(err => {console.error(err);});
+	const configPath = await appCacheDir();
+	await remove(configPath + "/temp.7z").catch(err => {console.log("Temporary game archive was not found. Nothing to delete!");});
+	if(patchTimestamp != 0) {
+		await remove(configPath + "/patch_" + patchTimestamp + ".7z").catch(err => {console.log("Temporary patch archive was not found. 🤨");});
+		await remove(configPath + "/patch_" + patchTimestamp).catch(err => {console.log("Temporary patch folder was not found. 🤨");});
+	}
 }
 
 library.changeUpdatingGameState = async function(state) {
@@ -180,15 +205,18 @@ library.changePendingUpdateState = async function(state) {
 library.openOrInstallGame = async function() {
 	if(isGameRunning) return;
 	if(isPendingUpdate) return library.updateGame();
+	clearInterval(gameCheckInterval);
+	const settings = library.getSettings();
 	await library.changeIsGameStartingState(true);
-	await open("GreenCatsServer.exe").then(res => {
+	await open(await join(settings.resource_path, settings.game_exe)).then(res => {
 		library.changeIsGameStartingState(false);
 		library.changeIsGameRunningState(true);
+		setTimeout(() => {const gameCheckInterval = setInterval(() => library.checkProcess(settings.game_exe), 500)}, 1250);
 	}).catch(err => {
 		library.changeIsGameStartingState(false);
 		library.changeIsGameRunningState(false);
 		console.log("Failed to run game:", err);
-		library.updateGame();
+		library.installGame();
 	})
 }
 
@@ -234,7 +262,10 @@ library.patchGame = async function(patchTimestamp) {
 					if(stdout === null) {
 						console.log('Patching ' + patchTimestamp + '...');
 						const patchFiles = await library.recursiveReadDir(patchFolderPath, patchFolderPath);
+						recursive_check = [];
 						var i = 0;
+						const patchedFiles = [];
+						const deletedFiles = [];
 						for(i = 0; i < patchFiles.length; i++) {
 							var patchFunction = patchFiles[i].slice(-2);
 							var patchFile = patchFiles[i].slice(0, patchFiles[i].length - 2);
@@ -245,40 +276,44 @@ library.patchGame = async function(patchTimestamp) {
 									const check = await Command.create("bin/hpatch.exe", [patchPath, fullPatchPath, patchPath + "_new"], { encoding: "utf-8" }).execute();
 									await remove(patchPath).catch(err => {console.error(err);});
 									await rename(patchPath + "_new", patchPath).catch(err => {console.error(err);});
+									patchedFiles.push(patchFile);
 									console.log("Patched", patchFile);
 									break;
 								case '.m':
-									await invoke('download_file', { url: settings.updates_api_url + "download/" + patchFile, tempPath: patchPath});
+									await invoke('download_file', { url: settings.updates_api_url + "download/" + btoa(encodeURIComponent(patchFile)), tempPath: patchPath});
+									patchedFiles.push(patchFile);
 									console.log("Downloaded", patchFile);
 									break;
 								case '.d':
 									await remove(patchPath).catch(err => {console.error(err);});
+									deletedFiles.push(patchFile);
 									console.log("Removed", patchFile);
 									break;
 							}
 						}
+						if(patchedFiles.length > 0) await library.addFilesToSQL(patchedFiles);
+						if(deletedFiles.length > 0) await library.removeFilesFromSQL(deletedFiles);
+						await library.removeEmptyFolders();
+						game_folders = [];
+						library.cleanTemporaryFiles(patchTimestamp);
 						r(true);
 					} else {
 						console.error('Failed extracting archive:', err);
-						library.changeUpdatingGameState(false);
 						library.cleanTemporaryFiles();
 						r(false);
 					}
 				}).catch(err => {				
 					console.error('Failed extracting archive:', err);
-					library.changeUpdatingGameState(false);
 					library.cleanTemporaryFiles();
 					r(false);
 				});
 			} else {
 				console.error('Failed downloading archive:', stdout);
-				library.changeUpdatingGameState(false);
 				library.cleanTemporaryFiles();
 				r(false);
 			}
 		}).catch(err => {
 			console.error('Failed downloading archive:', err);
-			library.changeUpdatingGameState(false);
 			library.cleanTemporaryFiles();
 			r(false);
 		});
@@ -287,20 +322,124 @@ library.patchGame = async function(patchTimestamp) {
 
 library.recursiveReadDir = async function(parent, initialParent) {
 	const dirEntries = await readDir(parent, { baseDir: BaseDirectory.Cache });
-	const patchFiles = [];
 	for(const entry of dirEntries) {
-		const onlyFilePath = parent.substr(initialParent.length);
-		patchFiles.push(onlyFilePath + entry.name);
+		const onlyFilePath = parent.substr(initialParent.length + await sep().length);
 		if(entry.isDirectory) {
+			const folderPath = onlyFilePath.length != 0 ? await join(onlyFilePath, entry.name) : entry.name;
+			game_folders.push(folderPath);
 			const dir = await join(parent, entry.name);
-			library.recursiveReadDir(dir, initialParent);
+			await library.recursiveReadDir(dir, initialParent);
+		} else {
+			const filePath = onlyFilePath.length != 0 ? await join(onlyFilePath, entry.name) : entry.name;
+			recursive_check.push(filePath);
 		}
 	}
-	return patchFiles;
+	return recursive_check;
+}
+
+library.addFolderToSQL = async function(folder) {
+	const allFiles = await library.recursiveReadDir(folder, folder);
+	await library.addFilesToSQL(allFiles);
+	await library.addGameFoldersToSQL(game_folders);
+	recursive_check = game_folders = [];
+}
+
+library.addFilesToSQL = async function(allFiles) {
+	const settings = library.getSettings();
+	var i = 0;
+	for(i = 0; i < allFiles.length; i++) {
+		const fileRelativePath = allFiles[i];
+		const md5 = await window.__TAURI__.core.invoke('get_file_md5', {filePath: await join(settings.resource_path, fileRelativePath)}) ?? 'MD5 failed';
+		await db.execute("INSERT INTO files (file, md5) VALUES($1, $2) ON CONFLICT(file) DO UPDATE SET md5 = $2", [fileRelativePath, md5]);
+	}
+}
+
+library.addGameFoldersToSQL = async function(allFolders) {
+	const settings = library.getSettings();
+	var i = 0;
+	for(i = 0; i < allFolders.length; i++) {
+		const folderRelativePath = allFolders[i];
+		await db.execute("INSERT INTO folders (folder) VALUES($1) ON CONFLICT(folder) DO UPDATE SET folder = $1", [folderRelativePath]);
+	}
+}
+
+library.removeFilesFromSQL = async function(allFiles) {
+	const deletedFilesString = "'" + allFiles.join("','") + "'";
+	await db.execute("DELETE FROM files WHERE file IN (" + deletedFilesString + ")");
+}
+
+library.uninstallGame = async function() {
+	if(window.isUpdatingGame) return;
+	const settings = library.getSettings();
+	console.log('Deleting game...');
+	library.changeUpdatingGameState(true);
+	const gameFiles = await db.select("SELECT file FROM files");
+	var i = 0;
+	for(i = 0; i < gameFiles.length; i++) {
+		const gameFile = gameFiles[i].file;
+		await remove(await join(settings.resource_path, gameFile)).catch(err => console.error("File " + gameFile + " was not found."));
+	}
+	const gameFolders = await db.select("SELECT folder FROM folders");
+	var i = 0;
+	for(i = 0; i < gameFolders.length; i++) game_folders.push(gameFolders[i].folder);
+	await library.removeEmptyFolders();
+	await db.execute("DELETE FROM files");
+	await db.execute("DELETE FROM folders");
+	localStorage.update_time = 0;
+	console.log('Game was successfully deleted! ...');
+	library.changePendingUpdateState(true);
+	library.changeUpdatingGameState(false);
+	library.checkUpdates();
+}
+
+library.removeEmptyFolders = async function() {
+	const settings = library.getSettings();
+	var i = game_folders.length - 1;
+	for(i = game_folders.length - 1; i >= 0; i--) {
+		const folderPath = game_folders[i];
+		remove(await join(settings.resource_path, folderPath), { recursive: false }).catch(err => console.error("Folder " + folderPath + " is not empty/was not found."));
+	}
+}
+
+library.checkProcess = async function(process) {
+	console.log("Game checked...");
+	return new Promise(resolve => {
+		invoke("check_processes", {process: process}).then(r => {
+			library.changeIsGameRunningState(true);
+			resolve(true)
+		}).catch(e => {
+			library.changeIsGameRunningState(false);
+			resolve(false)
+		});
+	});
+}
+
+library.verifyGameFilesIntegrity = async function() {
+	if(window.isUpdatingGame) return;
+	const settings = library.getSettings();
+	console.log("Verifying game files integrity...");
+	library.changeUpdatingGameState(true);
+	const gameFiles = await db.select("SELECT * FROM files");
+	var i = 0;
+	const failedFiles = [];
+	for(i = 0; i < gameFiles.length; i++) {
+		const gameFile = gameFiles[i].file;
+		const md5 = await window.__TAURI__.core.invoke('get_file_md5', {filePath: await join(settings.resource_path, gameFile)});
+		if(gameFiles[i].md5 != md5) failedFiles.push(gameFile);
+	}
+	if(failedFiles.length == 0) {
+		library.changeUpdatingGameState(false);
+		console.log('All files are fine!');
+	} else {
+		console.log(failedFiles);
+		// Потом закончу
+	}
 }
 
 library.styles = style;
 
 library.initializeEvents();
+
+//window.lib = library;
 
 export default library;
