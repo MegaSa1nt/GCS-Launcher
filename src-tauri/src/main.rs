4d8055976203192ff68be085f1e14e7d8ce65d8b
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-27 06:24:21 +0300

    Updater

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use reqwest::header::CONTENT_TYPE;
-use std::ffi::OsStr;
 use std::fs::File;
-use std::io::Write;
 use std::path::Path;
-use std::path::PathBuf;
-use sysinfo::System;
-use tauri::Manager;
-
-use chksum_md5;
+use std::io::Write;
 
 #[tauri::command]
 async fn download_file(url: String, temp_path: String) -> Result<(), String> {
@@ -33,71 +26,14 @@ async fn unpack_archive(archive_path: String, extract_path: String) -> Result<()
     Ok(())
 }
 
-#[tauri::command]
-async fn get_file_md5(file_path: String) -> Result<String, String> {
-    let path = PathBuf::from(file_path);
-	if path.exists() {
-		let digest = chksum_md5::chksum(path).expect("MD5 failed");
-		Ok(digest.to_hex_lowercase())
-	} else {
-		Err("Path doesn't exist".to_string())
-	}
-}
-
-#[tauri::command]
-async fn check_processes(process: String) -> Result<(), String> {
-    let s = System::new_all();
-    let os_str = OsStr::new(&process);
-    for _process in s.processes_by_exact_name(&os_str) {
-        return Ok(());
-    }
-    return Err("No process with that name was found".to_string());
-}
-
-#[tauri::command]
-async fn download_archive(url: String, temp_path: String, files: String) -> Result<(), String> {
-    let client = reqwest::Client::new();
-    let response = client
-        .post(&url)
-        .header(CONTENT_TYPE, "application/json")
-        .body(files)
-        .send()
-        .await
-        .expect("Failed downloading file");
-    let body = response.bytes().await.expect("Failed downloading file");
-    let path = Path::new(&temp_path);
-
-    let file = match File::create(&path) {
-        Err(why) => Err(why.to_string()),
-        Ok(file) => Ok(file),
-    };
-    file?
-        .write_all(&body.to_vec())
-        .expect(&"Failed writing file".to_string());
-    Ok(())
-}
-
 fn main() {
-	let mut builder = tauri::Builder::default();
-    #[cfg(desktop)]
-    {
-        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
-			let _ = app.get_webview_window("main")
-				.expect("no main window")
-				.set_focus();
-        }));
-    }
-    builder
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_sql::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
+    tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![
+        .plugin(tauri_plugin_shell::init())
+		.invoke_handler(tauri::generate_handler![
             download_file,
-            unpack_archive,
-            get_file_md5,
-            check_processes,
-            download_archive
+            unpack_archive
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ src/script.js
@@ -0,0 +1,62 @@
+// This app was done very fast, so be sure that it has some poor code
+
+const appWindow = window.__TAURI__.window.getCurrentWindow();
+const text = document.getElementById("ph1");
+
+appWindow.setMaximizable(false);
+appWindow.setResizable(false);
+appWindow.setShadow(false);
+
+const language = {
+	loading: "Downloading",
+	updating: "Updating",
+	done: "Done"
+}
+
+const getSettings = async function() {
+	const resourcePath = await window.__TAURI__.path.resourceDir();
+	return {
+		updates_api_url: "https://updates.gcs.icu/",
+		launcher_exe: "launcher.exe",
+		
+		resource_path: resourcePath
+	}
+}
+
+const installLauncher = async function() {
+	const settings = await getSettings();
+	const configPath = await window.__TAURI__.path.resolve(await window.__TAURI__.path.appCacheDir() + "/launcher.7z");
+	console.log('Starting downloading launcher...');
+	text.innerHTML = language.loading;
+	window.__TAURI__.core.invoke('download_file', { url: settings.updates_api_url + "download/launcher/0", tempPath: configPath}).then(stdout => {
+		if(stdout === null) {
+			console.log('Unpacking launcher...');
+			text.innerHTML = language.updating;
+			window.__TAURI__.core.invoke("unpack_archive", { archivePath: configPath, extractPath: settings.resource_path}).then(async function(stdout) {
+				if(stdout === null) {
+					window.__TAURI__.shell.open(settings.launcher_exe).then(res => {
+						text.innerHTML = language.done;
+						window.__TAURI__.process.exit(0);
+					}).catch(err => {				
+						console.error('Failed extracting archive:', err);
+						window.__TAURI__.process.exit(0);
+					});
+				} else {
+					console.error('Failed extracting archive:', err);
+					window.__TAURI__.process.exit(0);
+				}
+			}).catch(err => {				
+				console.error('Failed extracting archive:', err);
+				window.__TAURI__.process.exit(0);
+			});
+		} else {
+			console.error('Failed downloading archive:', stdout);
+			window.__TAURI__.process.exit(0);
+		}
+	}).catch(err => {
+		console.error('Failed downloading archive:', err);
+		window.__TAURI__.process.exit(0);
+	});
+}
+
+installLauncher();
