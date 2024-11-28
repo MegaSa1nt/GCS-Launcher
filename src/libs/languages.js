81a342498f4c7721bf63f532a2920be9fdc79852
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-28 16:59:57 +0300

    Add Vietnamese to launcher

--- src/libs/languages.js
+++ src/libs/languages.js
@@ -1,12 +1,14 @@
 import en from './languages/en.json';
 import ru from './languages/ru.json';
 import fr from './languages/fr.json';
+import vi from './languages/vi.json';
 // Not most efficient way, but thats everything i can do rn
 
 const strings = {
 	en: en,
 	ru: ru,
-	fr: fr
+	fr: fr,
+	vi: vi
 }
 
 export default strings;
