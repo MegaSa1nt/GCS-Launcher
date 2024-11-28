66f3ea15c05f176ad91457510700533f8c99c810
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2024-11-28 16:57:56 +0300

    Add Malay to launcher

--- src/libs/languages.js
+++ src/libs/languages.js
@@ -1,12 +1,14 @@
 import en from './languages/en.json';
 import ru from './languages/ru.json';
 import fr from './languages/fr.json';
+import bm from './languages/bm.json';
 // Not most efficient way, but thats everything i can do rn
 
 const strings = {
 	en: en,
 	ru: ru,
-	fr: fr
+	fr: fr,
+	bm: bm
 }
 
 export default strings;
