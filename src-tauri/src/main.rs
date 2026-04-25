57e3f7af9df32094a3d3372a9d66f03dd0ae21e6
MegaSa1nt <49565695+MegaSa1nt@users.noreply.github.com>
2026-04-26 03:31:20 +0500

    Updater 2.1.0
    Fixed updating logic

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -76,6 +76,7 @@ fn main() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_download::init())
 		.invoke_handler(tauri::generate_handler![
             download_file,
             unpack_archive
--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 // This app was done very fast, so be sure that it has some poor code
 
 const appWindow = window.__TAURI__.window.getCurrentWindow();
-const text = document.getElementById("ph1");
 
 appWindow.setMaximizable(false);
 appWindow.setResizable(false);
@@ -10,53 +9,136 @@ appWindow.setShadow(false);
 const language = {
 	loading: "Downloading",
 	updating: "Updating",
-	done: "Done"
+	done: "Done",
+	error: "Error"
 }
 
-const getSettings = async function() {
+const getSettings = async () => {
 	const resourcePath = await window.__TAURI__.path.resourceDir();
+	const separator = await window.__TAURI__.path.sep();
+	
 	return {
 		updates_api_url: "https://updates.example.com/",
 		launcher_exe: "launcher.exe",
 		
-		resource_path: resourcePath
+		resource_path: resourcePath,
+		separator: separator
 	}
 }
 
-const installLauncher = async function() {
-	const settings = await getSettings();
-	const configPath = await window.__TAURI__.path.resolve(await window.__TAURI__.path.appCacheDir() + "/launcher.zip");
-	console.log('Starting downloading launcher...');
-	text.innerHTML = language.loading;
-	window.__TAURI__.core.invoke('download_file', { url: `${settings.updates_api_url}download/pc-launcher/0`, tempPath: configPath}).then(async (stdout) => {
-		if(stdout === null) {
-			console.log('Unpacking launcher...');
-			text.innerHTML = language.updating;
-			window.__TAURI__.core.invoke("unpack_archive", { archivePath: configPath, outputPath: settings.resource_path + await window.__TAURI__.path.sep()}).then(async function(stdout) {
-				if(stdout === null) {
-					window.__TAURI__.shell.open(settings.launcher_exe).then(res => {
-						text.innerHTML = language.done;
-						window.__TAURI__.process.exit(0);
-					}).catch(err => {				
-						console.error('Failed extracting archive:', err);
-						window.__TAURI__.process.exit(0);
-					});
-				} else {
-					console.error('Failed extracting archive:', err);
-					window.__TAURI__.process.exit(0);
-				}
-			}).catch(err => {				
-				console.error('Failed extracting archive:', err);
-				window.__TAURI__.process.exit(0);
-			});
-		} else {
-			console.error('Failed downloading archive:', stdout);
-			window.__TAURI__.process.exit(0);
-		}
-	}).catch(err => {
-		console.error('Failed downloading archive:', err);
-		window.__TAURI__.process.exit(0);
+const downloadFile = async (url, savePath) => {
+	return new Promise(async (resolve, reject) => {
+		const file = await window.__TAURI__.core.invoke("plugin:download|create", {path: savePath, url: url}).catch((err) => reject(err));
+		
+		window.__TAURI__.event.listen("tauri-plugin-download:changed", (updatedDownload) => {
+			console.log(updatedDownload);
+			if(updatedDownload.payload.status.toLowerCase() == 'completed') return resolve(true);
+		});
+	   
+		await window.__TAURI__.core.invoke("plugin:download|start", {path: savePath}).catch((err) => reject(err));
+		
+		const checkInterval = setInterval(async () => { // For some reason plugin-download sometimes doesn't send "completed" event
+			const downloadsList = await window.__TAURI__.core.invoke("plugin:download|list").catch((err) => reject(err));
+			
+			if(!downloadsList.length) {
+				clearInterval(checkInterval);
+				return resolve(true);
+			}
+		}, 100);
 	});
 }
 
-installLauncher();
+const getTimestamp = async () => {
+	return Math.floor(new Date().getTime() / 1000);
+}
+
+const installer = {};
+
+installer.install = async () => {
+	const settings = await getSettings();
+	const launcherPath = await window.__TAURI__.path.resolve(await window.__TAURI__.path.appCacheDir() + "/launcher.zip");
+	
+	text.innerHTML = language.loading;
+	console.log('Downloading launcher...');
+	await installer.downloadUpdate(launcherPath).catch((err) => installer.showError(err));
+	if(isError) return;
+	
+	text.innerHTML = language.updating;
+	console.log('Extracting update...');
+	await installer.extractArchive(launcherPath).catch((err) => installer.showError(err));
+	if(isError) return;
+	
+	text.innerHTML = language.done;
+	console.log('Opening launcher...');
+	await installer.openLauncher().catch((err) => installer.showError(err));
+	if(isError) return;
+	
+	setTimeout(() => window.__TAURI__.process.exit(0), 100);
+}
+
+installer.showError = async (error) => {
+	window.isError = true;
+	
+	const timestamp = await getTimestamp();
+	text.innerHTML = language.error;
+	
+	icon.src = "res/x.svg";
+	icon.classList.remove("spin");
+	
+	console.error("Error has occured:", error);
+	
+	const logPath = await window.__TAURI__.path.resolve(`logs`);
+	const logFilePath = await window.__TAURI__.path.resolve(logPath + `/updater-${timestamp}.log`);
+	
+	if(!await window.__TAURI__.fs.exists(logPath)) await window.__TAURI__.fs.mkdir(logPath);
+	
+	const logFile = await window.__TAURI__.fs.create(logFilePath);
+	await logFile.write(new TextEncoder().encode(error.toString()));
+	await logFile.close();
+	
+	setTimeout(() => window.__TAURI__.process.exit(0), 4000);
+}
+
+installer.downloadUpdate = (path) => {
+	return new Promise(async (resolve, reject) => {
+		const settings = await getSettings();
+		
+		await downloadFile(`${settings.updates_api_url}download/pc-launcher/0`, path).catch((err) => reject(err));
+		
+		return resolve(true);
+	});
+}
+
+installer.extractArchive = async (path) => {
+	return new Promise(async (resolve, reject) => {
+		const settings = await getSettings();
+	
+		window.__TAURI__.core.invoke("unpack_archive", { archivePath: path, outputPath: settings.resource_path + settings.separator})
+			.then((stdout) => {
+				if(stdout !== null) return reject(stdout);
+				
+				return resolve(true);
+			})
+			.catch((err) => reject(err));		
+	});
+}
+
+installer.openLauncher = async () => {
+	return new Promise(async (resolve, reject) => {
+		const settings = await getSettings();
+	
+		await window.__TAURI__.shell.open(settings.launcher_exe).catch((err) => reject(err));
+		
+		return resolve(true);
+	});
+}
+
+window.addEventListener('load', () => {
+	window.text = document.getElementById("loadingText");
+	window.icon = document.getElementById("loadingIcon");
+	window.isError = false;
+	
+	installer.install();
+});
+
+window.installer = installer;
